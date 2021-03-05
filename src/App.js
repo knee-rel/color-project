6bf30d1d9387f4835e478b732aa58d2c6121525c
knee-rel <nirelmarieibarra@gmail.com>
2021-03-05 08:29:20 +0800

    added unstyled slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React, { Component } from "react";
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div>
-      <Palette {...seedColors[2]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
