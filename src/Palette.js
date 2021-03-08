a90ae98e64b5f61f4a74e76e61ae7452a1616151
knee-rel <nirelmarieibarra@gmail.com>
2021-03-08 16:29:30 +0800

    added snackbar and footer

--- src/Palette.js
+++ src/Palette.js
@@ -20,10 +20,10 @@ class Palette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => {
-      return <ColorBox background={color[format]} name={color.name} />;
+      return <ColorBox background={color[format]} name={color.name} key={color.id} />;
     });
     return (
       <div className="Palette">
@@ -33,6 +33,10 @@ class Palette extends Component {
           handleChange={this.changeFormat}
         />
         <div className="Palette-colors">{colorBoxes}</div>
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
